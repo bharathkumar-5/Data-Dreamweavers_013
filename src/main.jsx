d591407c139bc53ae1fdd8fa040f937e6826376c
Bharath Marley <bharathmarley103@gmail.com>
2024-11-18 15:26:36 +0530

    Features updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
@@ -7,7 +7,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <ChakraProvider>
     <BrowserRouter>
       <GoogleOAuthProvider clientId="807264241801-pqbudnduceo9pi6hc6jmbcqndnr54ma4.apps.googleusercontent.com">
@@ -16,5 +16,5 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
     </ChakraProvider>
     
-  </StrictMode>
+  </>
 );
